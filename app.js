7f05505cb1fa861e6984383b9dbba80cc6290eb0
alffox <alfonso.crisci.it@gmail.com>
2019-08-05 11:17:24 +0200

    refactor: Create server with core NodeJS http

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // const helpers = require('./helpers');
 const { sum } = require('./helpers');
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end("hello world from nodejs");
+});
+
+server.listen(3000);
+
 const total = sum(10, 200);
 
 console.log("Total: " + total);
