a937fdfd25f44d119c6ea2550991f7e4b13da78c
alffox <alfonso.crisci.it@gmail.com>
2019-08-05 13:59:57 +0200

    Refactor: use epxress

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // const helpers = require('./helpers');
 const { sum } = require('./helpers');
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.end("hello world from nodejs");
-});
+// const server = http.createServer((req, res) => {
+//     res.end("hello world from nodejs");
+// });
 
-server.listen(3000);
+// server.listen(3000);
+
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("test from express")
+})
+
+app.listen(3000)
 
 const total = sum(10, 200);
 
